100f448d78e490d4c47b5aa7c941653d8dd81e2c
Matteo Francia <m.francia@unibo.it>
2024-01-19 16:41:38 +0100

    fixing the ci

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -exo
-curl -o data/dump-20210119.sql https://big.csr.unibo.it/projects/nosql-datasets/dump-20210119.sql
+curl -o data/dump-20210119.sql https://big.csr.unibo.it/projects/nosql-datasets/watering-forecasting-20240119.sql
