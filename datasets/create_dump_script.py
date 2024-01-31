7e1c10e6daba14f775209e6df5681c3b0ed7ceb4
w4bo <you@example.com>
2024-01-31 12:06:13 +0000

    fix database dump

--- datasets/create_dump_script.py
+++ datasets/create_dump_script.py
@@ -13,11 +13,11 @@ with open('dump-clean-pg.sh', 'w') as f:
     f.write('export PGPASSWORD={}\n'.format(config['OUT_PWD']))
     f.write('pg_dump -h {} -d {} -U {} -O -x -t dt_agent -t dt_field -t dt_time -t ft_measurement -f /data/measurement-dfm_$T.sql\n'.format(config['OUT_HOST'], config['OUT_DB'], config['OUT_USER']))
     f.write('pg_dump -h {} -d {} -U {} -O -x -t cimice_ft_captures -t cimice_dt_trap -t cimice_dt_time -f /data/cimice-dfm_$T.sql\n'.format(config['OUT_HOST'], config['OUT_DB'], config['OUT_USER']))
-    f.write('ls /data')
+    f.write('ls -las /data | grep *dfm*sql')
         
 with open('dump-clean-ora.sh', 'w') as f:
     f.write('#!/bin/bash\nset -exo\n')
     f.write('T=$(date +%Y%m%dT%H%M%S)\nexport ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe\nexport PATH=$PATH:$ORACLE_HOME/bin\n')
-    f.write('expdp {user}/{pwd}@{host}:{port}/{db} directory=oracle_dump dumpfile={user}-cube.dmp SCHEMAS={user}\n'.format(user=config['OUT_ORA1_USER'], host=config['OUT_ORA1_HOST'], port=config['OUT_ORA1_PORT'], pwd=config['OUT_ORA1_PWD'], db=config['OUT_ORA1_DB']))
-    f.write('expdp {user}/{pwd}@{host}:{port}/{db} directory=oracle_dump dumpfile={user}-cube.dmp SCHEMAS={user}\n'.format(user=config['OUT_ORA2_USER'], host=config['OUT_ORA2_HOST'], port=config['OUT_ORA1_PORT'], pwd=config['OUT_ORA2_PWD'], db=config['OUT_ORA2_DB']))
-    f.write('ls -las /data | grep dmp')
+    f.write('expdp {user}/{pwd}@{host}:{port}/{db} directory=oracle_dump dumpfile={user}-dfm_$T.dmp SCHEMAS={user}\n'.format(user=config['OUT_ORA1_USER'], host=config['OUT_ORA1_HOST'], port=config['OUT_ORA1_PORT'], pwd=config['OUT_ORA1_PWD'], db=config['OUT_ORA1_DB']))
+    f.write('expdp {user}/{pwd}@{host}:{port}/{db} directory=oracle_dump dumpfile={user}-dfm_$T.dmp SCHEMAS={user}\n'.format(user=config['OUT_ORA2_USER'], host=config['OUT_ORA2_HOST'], port=config['OUT_ORA1_PORT'], pwd=config['OUT_ORA2_PWD'], db=config['OUT_ORA2_DB']))
+    f.write('ls -las /data | grep *dfm*dmp')
