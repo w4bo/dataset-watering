6d826f49fedb4e2cff2f91eb08561b1e7ed76eda
w4bo <you@example.com>
2024-04-05 09:51:13 +0000

    align with local changes

--- datasets/create_dump_script.py
+++ datasets/create_dump_script.py
@@ -2,17 +2,16 @@ from shared import config
 from datetime import datetime
 
 formatted_date = datetime.now().strftime("%Y%m%dT%H%M%S")
+preamble = f'#!/bin/bash\nset -exo\nT="{formatted_date}"\n'
 
 with open('dump-orig.sh', 'w') as f:
-    f.write('#!/bin/bash\nset -exo\n')
-    f.write(f'T="{formatted_date}"\n')
-    f.write('pg_dump -h {} -d {} -U {} -O -x -t synthetic_data -t synthetic_field -t synthetic_scenario -t synthetic_field_scenario -t synthetic_scenario_arpae_data -t synthetic_scenario_water_data -f /data/watering-sim_$(date +%Y%m%dT%H%M%S).sql\n'.format(config['IN_HOST'], config['IN_DB'], config['IN_USER']))
-    f.write('pg_dump -h {} -d {} -U {} -O -x -t data_interpolated -t view_data_original -f /data/watering_$(date +%Y%m%dT%H%M%S).sql\n'.format(config['IN2_HOST'], config['IN2_DB'], config['IN2_USER']))
-    f.write('pg_dump -h {} -d {} -U {} -O -x -t test_dim_trap -t test_bridge_trap_acque_interne -t test_bridge_trap_case -t test_bridge_trap_uso_suolo -t test_fact_passive_monitoring_normalized -t test_dim_acque_interne -t test_dim_case -t test_dim_uso_suolo -f /data/cimice_$(date +%Y%m%dT%H%M%S).sql'.format(config['IN3_HOST'], config['IN3_DB'], config['IN3_USER']))
+    f.write(preamble)
+    f.write('pg_dump -h {} -d {} -U {} -O -x -t synthetic_data -t synthetic_field -t synthetic_scenario -t synthetic_field_scenario -t synthetic_scenario_arpae_data -t synthetic_scenario_water_data -f /data/watering-sim_$T.sql\n'.format(config['IN_HOST'], config['IN_DB'], config['IN_USER']))
+    f.write('pg_dump -h {} -d {} -U {} -O -x -t data_interpolated -t view_data_original -f /data/watering_$T.sql\n'.format(config['IN2_HOST'], config['IN2_DB'], config['IN2_USER']))
+    f.write('pg_dump -h {} -d {} -U {} -O -x -t test_dim_trap -t test_bridge_trap_acque_interne -t test_bridge_trap_case -t test_bridge_trap_uso_suolo -t test_fact_passive_monitoring_normalized -t test_dim_acque_interne -t test_dim_case -t test_dim_uso_suolo -f /data/cimice_$T.sql'.format(config['IN3_HOST'], config['IN3_DB'], config['IN3_USER']))
 
 with open('dump-clean-pg.sh', 'w') as f:
-    f.write('#!/bin/bash\nset -exo\n')
-    f.write(f'T={formatted_date}\n')
+    f.write(preamble)
     f.write('export PGPASSWORD={}\n'.format(config['OUT_PWD']))
     f.write((
                 'pg_dump -h {host} -d {db} -U {user} -O -x -t dt_agent -t dt_field -t dt_time -t ft_measurement -f /data/measurement-dfm_$T.sql\n' +
@@ -20,8 +19,7 @@ with open('dump-clean-pg.sh', 'w') as f:
             ).format(host=config['OUT_HOST'], db=config['OUT_DB'], user=config['OUT_USER']))
 
 with open('dump-clean-ora.sh', 'w') as f:
-    f.write('#!/bin/bash\nset -exo\n')
-    f.write(f'T="{formatted_date}"\n')
+    f.write(preamble)
     f.write('export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe\nexport PATH=$PATH:$ORACLE_HOME/bin\n')
     for i in [1, 2]: 
         f.write((
